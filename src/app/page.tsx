f4fd922dc1ca9f99b3cb12415678b53665bb668e
Jamova01 <jorgemova01@gmail.com>
2024-02-05 10:19:09 -0500

    [UPDATE]: Implementing contact form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,80 @@ export default function Home() {
       <Hero />
       <Products />
       <AlliedBrands />
+      <div className="container mx-auto mt-10" id="contact-us">
+        <h2 className="text-4xl font-bold text-center text-[#063B54] mb-6">
+          Contáctanos
+        </h2>
+        <div className="flex flex-col gap-4 text-center mb-6">
+          <p className="text-center mb-6">
+            Para contactarse con nosotros, por favor diligencie el siguiente
+            formulario.
+          </p>
+          <div>
+            <h3 className="text-lg font-semibold mb-2">Horarios de Atención</h3>
+            <p>Lunes a Viernes: 7:30am – 5pm</p>
+            <p>Sábados: 8am – 12:30pm</p>
+          </div>
+        </div>
+        <form className="max-w-md mx-auto">
+          <div className="mb-4">
+            <label
+              htmlFor="nombre"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Nombre
+            </label>
+            <input
+              type="text"
+              id="nombre"
+              name="nombre"
+              className="mt-1 p-2 w-full border rounded-md"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="mt-1 p-2 w-full border rounded-md"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="mensaje"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Mensaje
+            </label>
+            <textarea
+              id="mensaje"
+              name="mensaje"
+              rows={4}
+              className="mt-1 p-2 w-full border rounded-md"
+              required
+            ></textarea>
+          </div>
+
+          <div className="mb-4">
+            <button
+              type="submit"
+              className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+            >
+              Enviar
+            </button>
+          </div>
+        </form>
+      </div>
     </main>
   );
 }
