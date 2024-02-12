3c5a515f377175e9f6420a9bc00c47a3e7a53423
Jamova01 <jorgemova01@gmail.com>
2024-02-12 05:16:20 -0500

    [UPDATE]: Implementing whatsapp button

--- src/components/ui/whatsapp-button.tsx
+++ src/components/ui/whatsapp-button.tsx
@@ -2,7 +2,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 export const WhatsappButton = () => {
   return (
-    <div className="flex justify-center items-center px-2 py-2 xs:bottom-5 xs:right-5 md:bottom-10 md:right-10 rounded-main fixed z-50 bg-green-500 rounded-xl">
+    <div className="flex justify-center items-center px-2 py-2  bottom-10 right-10 rounded-main fixed z-50 bg-green-500 rounded-xl">
       <a
         href="https://wa.me/573113001827"
         target="_blank"
